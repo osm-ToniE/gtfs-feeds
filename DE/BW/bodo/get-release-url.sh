7f90814afa9002d747b25c3f8f1ed45193145789
Toni Erdmann <Toni.Erdmann@web.de>
2020-09-28 19:50:37 +0200

    complete URL

--- DE/BW/bodo/get-release-url.sh
+++ DE/BW/bodo/get-release-url.sh
@@ -4,7 +4,9 @@
 # get URL to download latest GTFS feed
 #
 
-GTFS_URL_INFO_ParseUrl='https://www.nvbw.de/open-data/fahrplandaten/fahrplandaten-mit-liniennetz'
+Base="https://www.nvbw.de"
+
+GTFS_URL_INFO_ParseUrl="$Base/open-data/fahrplandaten/fahrplandaten-mit-liniennetz"
 
 
 if [ "$1" = "-v" ]
@@ -16,7 +18,11 @@ fi
 
 if [ -f "parse_url.tmp" -a -s "parse_url.tmp" ]
 then
-    perl -n -e 'if ( m/Bodensee-Oberschwaben Verkehrsverbund.*?href="([^"]*)">gtfs gezipt/ ) { $url = $1, $url =~ s/\&amp;/\&/g; printf( "%s\n", $url ); }' parse_url.tmp
+    Uri=$(perl -n -e 'if ( m/Bodensee-Oberschwaben Verkehrsverbund.*?href="([^"]*)">gtfs gezipt/ ) { $url = $1, $url =~ s/\&amp;/\&/g; printf( "%s\n", $url ); }' parse_url.tmp)
+    if [ -n "$Uri" ]
+    then
+        echo "$Base$Uri"
+    fi
 else
     echo $(date "+%Y-%m-%d %H:%M:%S") "Temporary file: 'parse_url.tmp' does not exist or is empty" >> /dev/stderr
     exit 1
