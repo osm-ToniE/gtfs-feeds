20d9d03ff1e9026568e76bfe7f47cc710a706597
Toni Erdmann <Toni.Erdmann@web.de>
2020-09-28 19:38:00 +0200

    rename files (avoid '_') add get-feed-name.sh

--- DE/BW/bodo/cleanup.sh
+++ DE/BW/bodo/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# delete temporary files, retrieved GTFS data, ...
+# delete temporary files
 #
 
-rm -rf ./parse_date.tmp ./parse_url.tmp ./20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]/
+rm -rf ./parse_date.tmp ./parse_url.tmp
