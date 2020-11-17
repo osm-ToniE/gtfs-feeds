fb14be397f44bd23fedfbdf17bb0d76faf954237
Toni Erdmann <Toni.Erdmann@web.de>
2020-11-17 17:24:47 +0100

    add KVV to automated GTFS check/download

---
+++ DE/BW/KVV/get-feed-name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# print name of GTFS feed
+#
+
+echo "DE-BW-KVV"
---
+++ DE/BW/KVV/get-release-url.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+echo "https://projekte.kvv-efa.de/GTFS/google_transit.zip"
