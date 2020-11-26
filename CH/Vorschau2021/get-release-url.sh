f7e663ec2021c423b09c11acedf7c55e9e575f84
Toni Erdmann <Toni.Erdmann@web.de>
2020-11-26 11:04:37 +0100

    add code for CH-* for issue #7

---
+++ CH/Vorschau2021/get-release-url.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+PERMALINK="https://opentransportdata.swiss/de/dataset/timetable-2021-gtfs2020/permalink"
+
+LOCATION=$(curl -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+if [ -n "$LOCATION" ]
+then
+    if [ "$(echo $LOCATION | grep -c '^https://opentransportdata.swiss/dataset/')" == 1 ]
+    then
+        RELEASE_URL=$LOCATION
+    fi
+fi
+
+echo $RELEASE_URL
