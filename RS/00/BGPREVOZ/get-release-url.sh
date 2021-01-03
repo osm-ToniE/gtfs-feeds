15740c8d8e49ed333dc02d831b8c0d5ce2a3770c
Toni Erdmann <Toni.Erdmann@web.de>
2021-01-03 17:24:45 +0100

    fix location of GTFS data

--- RS/00/BGPREVOZ/get-release-url.sh
+++ RS/00/BGPREVOZ/get-release-url.sh
@@ -4,13 +4,13 @@
 # get URL to download latest GTFS feed
 #
 
-PERMALINK="https://data.gov.rs/sr/datasets/r/d26fcb2a-c869-49c4-a7ac-1f90d0f69046"
+PERMALINK="https://data.gov.rs/sr/datasets/r/b4df09a9-579f-46b0-826f-f397a51e3786"
 
 LOCATION=$(curl -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
 
 if [ -n "$LOCATION" ]
 then
-    if [ "$(echo $LOCATION | grep -c '^https://data.gov.rs/s/resources/gtfs/')" == 1 ]
+    if [ "$(echo $LOCATION | grep -c '^https://data.gov.rs/s/resources/gtfs/.*\.zip$')" == 1 ]
     then
         RELEASE_URL=$LOCATION
     fi
