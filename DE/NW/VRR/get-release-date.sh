fd69e3942b8580d6cb6a3e244b698943c4f9e526
Toni Erdmann <Toni.Erdmann@web.de>
2021-05-06 19:18:48 +0200

    adapt to changes and errors on VRR's web site

--- DE/NW/VRR/get-release-date.sh
+++ DE/NW/VRR/get-release-date.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 #
-# get URL to download latest GTFS feed
+# retrieve release date of latest GTFS feed in form "YYYY-MM-DD"
 #
 
-SCANURL="https://www.opendata-oepnv.de/ht/de/organisation/verkehrsverbuende/vrr/startseite?tx_vrrkit_view%5Bdataset_name%5D=soll-fahrplandaten-vrr&tx_vrrkit_view%5Bdataset_formats%5D%5B0%5D=ZIP&tx_vrrkit_view%5Baction%5D=details&tx_vrrkit_view%5Bcontroller%5D=View"
+RELEASE_URL=$(./get-release-url.sh)
 
-SCANDATE=$(curl -s $SCANURL -o - | \
-           egrep -i 'href="https://www.opendata-oepnv.de/dataset/.*/download/[0-9][0-9][0-9][0-9]_[0-9][0-9]_[0-9][0-9]_google_transit_opendata.zip"' | \
-           tail -1 | \
-           sed -e 's/^.*download\///i' \
-               -e 's/_google_transit_opendata\.zip.*$//')
-
-if [ -n "$SCANDATE" ]
+if [ -n "$RELEASE_URL" ]
 then
-    CONVEDDATE=$(echo $SCANDATE | sed -e 's/\([0-9][0-9][0-9][0-9]\)_\([0-9][0-9]\)_\([0-9][0-9]\)/\1-\2-\3/')
-    FIXEDDATE=$(date -d"$CONVEDDATE" '+%Y-%m-%d')
-    if [ "$(echo $FIXEDDATE | grep -c '^20[0-9][0-9]-[01][0-9]-[0123][0-9]$')" == 1 ]
+    LAST_MODIFIED=$(curl -sI $RELEASE_URL | fgrep -i 'last-modified:' | sed -e 's/^last-modified:\s*//i')
+
+    if [ -n "$LAST_MODIFIED" ]
     then
-        RELEASE_DATE=$FIXEDDATE
+        result=$(date -d "$LAST_MODIFIED" '+%Y-%m-%d')
+        if [ "$(echo $result | grep -c '^20[0-9][0-9]-[01][0-9]-[0123][0-9]$')" == 1 ]
+        then
+            RELEASE_DATE=$result
+        fi
     fi
 fi
 
--- DE/NW/VRR/get-release-url.sh
+++ DE/NW/VRR/get-release-url.sh
@@ -7,10 +7,11 @@
 SCANURL="https://www.opendata-oepnv.de/ht/de/organisation/verkehrsverbuende/vrr/startseite?tx_vrrkit_view%5Bdataset_name%5D=soll-fahrplandaten-vrr&tx_vrrkit_view%5Bdataset_formats%5D%5B0%5D=ZIP&tx_vrrkit_view%5Baction%5D=details&tx_vrrkit_view%5Bcontroller%5D=View"
 
 LOCATION=$(curl -s $SCANURL -o - | \
-           egrep -i 'href="https://www.opendata-oepnv.de/dataset/.*/download/[0-9][0-9][0-9][0-9]_[0-9][0-9]_[0-9][0-9]_google_transit_opendata.zip"' | \
+           egrep -i 'href="https://.*opendata-oepnv.de/dataset/.*/download/.*google_transit_opendata.zip"' | \
            tail -1 | \
            sed -e 's/^.*href="https:/https:/i' \
-               -e 's/_google_transit_opendata\.zip.*$/_google_transit_opendata.zip/')
+               -e 's/www\.www\./www./' \
+               -e 's/google_transit_opendata\.zip.*$/google_transit_opendata.zip/')
 
 if [ -n "$LOCATION" ]
 then
