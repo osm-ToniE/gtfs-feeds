e93fa57cac3b4da5171080663acc399a0ac5c5a4
Toni Erdmann <Toni.Erdmann@web.de>
2021-10-18 14:11:19 +0200

    adapt to changes in url

--- HR/21/ZET/get-release-url.sh
+++ HR/21/ZET/get-release-url.sh
@@ -7,7 +7,7 @@
 SCANURL="https://www.zet.hr/odredbe/datoteke-u-gtfs-formatu/669"
 
 LOCATION=$(curl -s $SCANURL -o - | \
-           egrep -i 'href="https://www.zet.hr/UserDocsImages/Dokumenti i obrasci za preuzimanje/(GTFS\s*-\s*[0-9]+\.[0-9]+\.[0-9][0-9][0-9][0-9]\..*|scheduled-.*?)\.zip' | \
+           egrep -i 'href="https://www.zet.hr/UserDocsImages/Dokumenti i obrasci za preuzimanje/(GTFS.*[0-9]+\.[0-9]+\.[0-9][0-9][0-9][0-9]\..*|scheduled-.*?)\.zip' | \
            head -1 | \
            sed -e 's/^.*href="https:/https:/i' \
                -e 's/".*$//'                   \
