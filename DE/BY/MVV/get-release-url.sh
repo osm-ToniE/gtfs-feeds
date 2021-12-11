2911bd88c1a7899424bded149a4ccbf5bd2c0b42
Toni Erdmann <Toni.Erdmann@web.de>
2021-12-11 12:53:21 +0100

    update feed URL of MVV

--- DE/BY/MVV/get-release-url.sh
+++ DE/BY/MVV/get-release-url.sh
@@ -4,4 +4,5 @@
 # get URL to download latest GTFS feed
 #
 
-echo "https://www.mvv-muenchen.de/fileadmin/mediapool/02-Fahrplanauskunft/03-Downloads/openData/mvv_gtfs.zip"
+#echo "https://www.mvv-muenchen.de/fileadmin/mediapool/02-Fahrplanauskunft/03-Downloads/openData/mvv_gtfs.zip"
+echo "https://www.opendata-oepnv.de/dataset/17065229-c3fd-46d7-84a9-aae55aadbf40/resource/208c55dd-15d6-4ea0-abd7-8433d64180ed/download/mvv_gtfs.zip"
