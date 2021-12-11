d2e7e6d41017d9237de3fb07003e0010a6691a98
Toni Erdmann <Toni.Erdmann@web.de>
2021-12-11 19:30:33 +0100

    update to changed link names

--- DE/NW/VRR/get-release-url.sh
+++ DE/NW/VRR/get-release-url.sh
@@ -7,11 +7,11 @@
 SCANURL="https://www.opendata-oepnv.de/ht/de/organisation/verkehrsverbuende/vrr/startseite?tx_vrrkit_view%5Bdataset_name%5D=soll-fahrplandaten-vrr&tx_vrrkit_view%5Bdataset_formats%5D%5B0%5D=ZIP&tx_vrrkit_view%5Baction%5D=details&tx_vrrkit_view%5Bcontroller%5D=View"
 
 LOCATION=$(curl -s $SCANURL -o - | \
-           egrep -i 'href="https://.*opendata-oepnv.de/dataset/.*/download/.*google_transit_opendata.zip"' | \
+           egrep -i 'href="https://.*opendata-oepnv.de/dataset/.*/download/.*\.zip"'  | \
            tail -1 | \
            sed -e 's/^.*href="https:/https:/i' \
                -e 's/www\.www\./www./' \
-               -e 's/google_transit_opendata\.zip.*$/google_transit_opendata.zip/')
+               -e 's/\.zip.*$/.zip/')
 
 if [ -n "$LOCATION" ]
 then
