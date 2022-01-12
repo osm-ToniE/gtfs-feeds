61c47f7816324ab8b39fd537e9fa5a2f2d810aae
Toni Erdmann <Toni.Erdmann@web.de>
2022-01-12 12:31:47 +0100

    adapt URL to new settings

--- DE/BE/VBB/get-release-url.sh
+++ DE/BE/VBB/get-release-url.sh
@@ -4,4 +4,4 @@
 # get URL to download latest GTFS feed
 #
 
-echo "https://www.vbb.de/fileadmin/user_upload/VBB/Dokumente/API-Datensaetze/GTFS.zip"
+echo "https://www.vbb.de/fileadmin/user_upload/VBB/Dokumente/API-Datensaetze/gtfs-mastscharf/GTFS.zip"
