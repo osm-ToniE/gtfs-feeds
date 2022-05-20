fa77692e01733242388d0c69f7e9937875dd0df5
Toni Erdmann <Toni.Erdmann@web.de>
2022-05-20 12:15:26 +0200

    download VV GTFS feed directly from MVV

--- DE/BY/MVV/get-release-url.sh
+++ DE/BY/MVV/get-release-url.sh
@@ -4,20 +4,20 @@
 # get URL to download latest GTFS feed
 #
 
-#echo "https://www.mvv-muenchen.de/fileadmin/mediapool/02-Fahrplanauskunft/03-Downloads/openData/mvv_gtfs.zip"
+echo "https://www.mvv-muenchen.de/fileadmin/mediapool/02-Fahrplanauskunft/03-Downloads/openData/mvv_gtfs.zip"
 
-SCANURL="https://www.opendata-oepnv.de/ht/de/organisation/verkehrsverbuende/mvv/startseite?tx_vrrkit_view%5Bdataset_name%5D=soll_fahrplandaten_mvv&tx_vrrkit_view%5Baction%5D=details&tx_vrrkit_view%5Bcontroller%5D=View"
+#SCANURL="https://www.opendata-oepnv.de/ht/de/organisation/verkehrsverbuende/mvv/startseite?tx_vrrkit_view%5Bdataset_name%5D=soll_fahrplandaten_mvv&tx_vrrkit_view%5Baction%5D=details&tx_vrrkit_view%5Bcontroller%5D=View"
 
-LOCATION=$(curl -s $SCANURL -o - | \
-           egrep -i 'href="https://.*opendata-oepnv.de/dataset/.*/download/.*\.zip"'  | \
-           tail -1 | \
-           sed -e 's/^.*href="https:/https:/i' \
-               -e 's/www\.www\./www./' \
-               -e 's/\.zip.*$/.zip/')
+#LOCATION=$(curl -s $SCANURL -o - | \
+#           egrep -i 'href="https://.*opendata-oepnv.de/dataset/.*/download/.*\.zip"'  | \
+#           tail -1 | \
+#           sed -e 's/^.*href="https:/https:/i' \
+#               -e 's/www\.www\./www./' \
+#               -e 's/\.zip.*$/.zip/')
 
-if [ -n "$LOCATION" ]
-then
-    RELEASE_URL=$LOCATION
-fi
+#if [ -n "$LOCATION" ]
+#then
+#    RELEASE_URL=$LOCATION
+#fi
 
-echo $RELEASE_URL
+#echo $RELEASE_URL
