33a785fd94aa913dc84bad5913af9f4a0d31efb0
Toni Erdmann <Toni.Erdmann@web.de>
2022-08-18 12:47:40 +0200

    update URL of latest GTFS release
    - now static address, no need to scan

--- HR/21/ZET/get-release-url.sh
+++ HR/21/ZET/get-release-url.sh
@@ -5,8 +5,8 @@
 #
 
 #SCANURL="https://www.zet.hr/odredbe/datoteke-u-gtfs-formatu/669"
-BASEURL="https://zet.hr"
-SCANURL="${BASEURL}/gtfs2"
+#BASEURL="https://zet.hr"
+#SCANURL="${BASEURL}/gtfs2"
 
 #LOCATION=$(curl -s $SCANURL -o - |
 #           egrep -i 'href="https://www.zet.hr/UserDocsImages/Dokumenti i obrasci za preuzimanje/(GTFS.*[0-9]+\.[0-9]+\.[0-9][0-9][0-9][0-9]\..*|scheduled-.*?)\.zip' | \
@@ -15,17 +15,19 @@ SCANURL="${BASEURL}/gtfs2"
 #               -e 's/".*$//'                   \
 #               -e 's/ /%20/g')
 
-LOCATION=$(curl -s $SCANURL -o - | \
-           sed -e 's/<\/a>/<\/a>\n/g' -e 's/<a>/<a>\n/g'  | \
-           egrep -i 'href="/gtfs-scheduled/scheduled-.*?\.zip">GTFS Static Data Latest' | \
-           head -1               | \
-           sed -e 's/^.*href="//i' \
-               -e 's/".*$//'       \
-               -e 's/ /%20/g')
+#LOCATION=$(curl -s $SCANURL -o - | \
+#           sed -e 's/<\/a>/<\/a>\n/g' -e 's/<a>/<a>\n/g'  | \
+#           egrep -i 'href="/gtfs-scheduled/scheduled-.*?\.zip">GTFS Static Data Latest' | \
+#           head -1               | \
+#           sed -e 's/^.*href="//i' \
+#               -e 's/".*$//'       \
+#               -e 's/ /%20/g')
 
-if [ -n "$LOCATION" ]
-then
-    RELEASE_URL="${BASEURL}$LOCATION"
-fi
+#if [ -n "$LOCATION" ]
+#then
+#    RELEASE_URL="${BASEURL}$LOCATION"
+#fi
+
+RELEASE_URL=https://zet.hr/gtfs-scheduled/latest
 
 echo $RELEASE_URL
