65a43da2ea84e0b32c0eb4f4e12ae51c770ba421
Toni Erdmann <Toni.Erdmann@web.de>
2022-09-22 12:40:37 +0200

    adapr script to changes on VMS' web page

--- DE/SN/VMS/get-release-date.sh
+++ DE/SN/VMS/get-release-date.sh
@@ -4,9 +4,9 @@
 # get URL to download latest GTFS feed
 #
 
-SCANURL="https://www.vms.de/vms/open-data-gtfs-daten/"
+SCANURL="https://www.vms.de/vms/service/downloads/"
 
-SCANDATE=$(curl -s $SCANURL -o - | egrep -i 'href="fileadmin/user_upload/GTFS-Daten/GTFS_VMS_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\.zip"' | sed -e 's/^.*GTFS_VMS_//i' -e 's/\.zip.*$//')
+SCANDATE=$(curl -s $SCANURL -o - | egrep -i 'href="https://www.vms.de/wp-content/uploads/.*/GTFS_VMS_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]+\.zip"' | tail -1 | sed -e 's/^.*GTFS_VMS_//i' -e 's/\.zip.*$//')
 
 if [ -n "$SCANDATE" ]
 then
--- DE/SN/VMS/get-release-url.sh
+++ DE/SN/VMS/get-release-url.sh
@@ -4,13 +4,13 @@
 # get URL to download latest GTFS feed
 #
 
-SCANURL="https://www.vms.de/vms/open-data-gtfs-daten/"
+SCANURL="https://www.vms.de/vms/service/downloads/"
 
-LOCATION=$(curl -s $SCANURL -o - | egrep -i 'href="fileadmin/user_upload/GTFS-Daten/GTFS_VMS_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]+\.zip"' | tail -1 | sed -e 's/^.*href="fileadmin/fileadmin/i' -e 's/\.zip.*$/.zip/')
+LOCATION=$(curl -s $SCANURL -o - | egrep -i 'href="https://www.vms.de/wp-content/uploads/.*/GTFS_VMS_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]+\.zip"' | tail -1 | sed -e 's/^.*href="//i' -e 's/\.zip.*$/.zip/')
 
 if [ -n "$LOCATION" ]
 then
-    RELEASE_URL="https://www.vms.de/$LOCATION"
+    RELEASE_URL="$LOCATION"
 fi
 
 echo $RELEASE_URL
