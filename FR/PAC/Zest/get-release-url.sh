457ee0e94190704717199d4b5665f7cd4c94d671
Toni Erdmann <Toni.Erdmann@web.de>
2023-01-14 17:06:27 +0100

    add GTFS feed FR-PAC-Zest

---
+++ FR/PAC/Zest/get-release-url.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+PERMALINK="https://www.data.gouv.fr/fr/datasets/r/72609821-2459-47fb-a63b-3dbbc0d96c92"
+
+LOCATION=$(curl -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+if [ -n "$LOCATION" ]
+then
+    if [ "$(echo $LOCATION | grep -c 'horaires-reseau-zest')" == 1 ]
+    then
+        RELEASE_URL=$LOCATION
+    fi
+fi
+
+echo $RELEASE_URL
