d43a6f448298c7c14d5ba42cfda78658356195fa
Toni Erdmann <Toni.Erdmann@web.de>
2023-01-24 12:58:25 +0100

    add PL-24-ZTM-Katowice

---
+++ PL/24/ZTM-Katowice/get-release-url.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+RELEASE_URL=$(curl -s https://otwartedane.metropoliagzm.pl/dataset/86b5ce0c-daea-4b40-bc60-af2c80477d21.jsonld | \
+              jq ' ."@graph"[]."dcat:accessURL"."@id"'                                                         | \
+              fgrep 'schedule'                                                                                 | \
+              sed -e 's/^"//' -e 's/"$//' -e 's/^\(.*\)\(schedule.*\)$/\2 \1/'                                 | \
+              sort -r                                                                                          | \
+              head -1                                                                                          | \
+              sed -e 's/^\(.*\) \(http.*\)$/\2\1/')
+
+if [ -n "$RELEASE_URL" ]
+then
+    echo $RELEASE_URL
+fi
