ea8b065f97a08e11d9b99245454509593c6554c5
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-03 12:35:38 +0100

    consider double response of HTTP/1.0 302 Moved Temporarily

--- FR/PAC/Lignes-d-Azur/get-release-url.sh
+++ FR/PAC/Lignes-d-Azur/get-release-url.sh
@@ -4,15 +4,24 @@
 # get URL to download latest GTFS feed
 #
 
-PERMALINK="http://opendata.nicecotedazur.org/data/dataset/export-quotidien-au-format-gtfs-du-reseau-de-transport-lignes-d-azur/resource/aacb4eea-d008-4b13-b17a-848b8ced7e03/download"
+PERMALINK="https://opendata.nicecotedazur.org/data/dataset/export-quotidien-au-format-gtfs-du-reseau-de-transport-lignes-d-azur/resource/aacb4eea-d008-4b13-b17a-848b8ced7e03/download"
 
 LOCATION=$(curl -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
 
 if [ -n "$LOCATION" ]
 then
-    if [ "$(echo $LOCATION | grep -c '//opendata.nicecotedazur.org/')" == 1 ]
+    if [ "$(echo $LOCATION | grep -c 'http://')" == 1 ]
     then
-        RELEASE_URL=$LOCATION
+        OTHER_LOCATION=$(curl -sI $LOCATION | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+        if [ "$(echo $OTHER_LOCATION | grep -c '//opendata.nicecotedazur.org/')" == 1 ]
+        then
+            RELEASE_URL=$OTHER_LOCATION
+        fi
+    else
+        if [ "$(echo $LOCATION | grep -c '//opendata.nicecotedazur.org/')" == 1 ]
+        then
+            RELEASE_URL=$LOCATION
+        fi
     fi
 fi
 
