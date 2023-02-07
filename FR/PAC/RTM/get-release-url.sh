ddc4ae13f33a6a63cad4c252104edcc29c5d151a
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-07 12:59:21 +0100

    add two feeds in FR-PAC
    - RTM for Marseille
    - Trans'Aglo for Manosque
      - the contents of the GTFS points to Avignon - is wrong

---
+++ FR/PAC/RTM/get-release-url.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+echo "https://tsvc2.pilote3.cityway.fr/api/Export/v1/GetExportedDataFile?ExportFormat=Gtfs&OperatorCode=RTM"
---
+++ FR/PAC/Trans-Agglo/get-release-url.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+echo ""
+
+#echo "https://trouver.datasud.fr/dataset/0c5ad935-272e-4d9e-b0db-cdcb1a5309b8/resource/0ca29750-b689-4580-afc0-4b127ee4b914/download/dlva.gtfs.zip"
