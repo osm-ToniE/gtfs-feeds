f1f7f79245dcf097df9f8a6f416738b06e0affff
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-07 18:39:10 +0100

    set url as correct

--- FR/PAC/Trans-Agglo/get-release-url.sh
+++ FR/PAC/Trans-Agglo/get-release-url.sh
@@ -4,6 +4,4 @@
 # get URL to download latest GTFS feed
 #
 
-echo ""
-
-#echo "https://trouver.datasud.fr/dataset/0c5ad935-272e-4d9e-b0db-cdcb1a5309b8/resource/0ca29750-b689-4580-afc0-4b127ee4b914/download/dlva.gtfs.zip"
+echo "https://trouver.datasud.fr/dataset/0c5ad935-272e-4d9e-b0db-cdcb1a5309b8/resource/0ca29750-b689-4580-afc0-4b127ee4b914/download/dlva.gtfs.zip"
