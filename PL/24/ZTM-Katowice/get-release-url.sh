9299298ccc3ff32cbb86586d3cf113781a928f39
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-09 13:58:21 +0100

    set --connect-timeout to 30
    - on everest, we will get no connection anyway, somewhere blocked

--- PL/24/ZTM-Katowice/get-release-url.sh
+++ PL/24/ZTM-Katowice/get-release-url.sh
@@ -4,12 +4,12 @@
 # get URL to download latest GTFS feed
 #
 
-RELEASE_URL=$(curl -s https://otwartedane.metropoliagzm.pl/dataset/86b5ce0c-daea-4b40-bc60-af2c80477d21.jsonld | \
-              jq ' ."@graph"[]."dcat:accessURL"."@id"'                                                         | \
-              fgrep 'schedule'                                                                                 | \
-              sed -e 's/^"//' -e 's/"$//' -e 's/^\(.*\)\(schedule.*\)$/\2 \1/'                                 | \
-              sort -r                                                                                          | \
-              head -1                                                                                          | \
+RELEASE_URL=$(curl --connect-timeout 30 -s https://otwartedane.metropoliagzm.pl/dataset/86b5ce0c-daea-4b40-bc60-af2c80477d21.jsonld | \
+              jq ' ."@graph"[]."dcat:accessURL"."@id"'                                                                              | \
+              fgrep 'schedule'                                                                                                      | \
+              sed -e 's/^"//' -e 's/"$//' -e 's/^\(.*\)\(schedule.*\)$/\2 \1/'                                                      | \
+              sort -r                                                                                                               | \
+              head -1                                                                                                               | \
               sed -e 's/^\(.*\) \(http.*\)$/\2\1/')
 
 if [ -n "$RELEASE_URL" ]
