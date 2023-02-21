8a6dc984b522915f31916dadf684dcad3c6b5bf0
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-21 17:24:19 +0100

    add GTFS-feed DE-SH-Landesweit

---
+++ DE/SH/Landesweit/get-release-url.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+SCANURL="https://opendata.schleswig-holstein.de/dataset/fahrplandaten"
+BASEURL="https://opendata.schleswig-holstein.de"
+
+LOCATION=$(curl -s $SCANURL -o - | egrep -i 'href="/dataset/.*/download/fahrplandaten.zip"' | tail -1 | sed -e 's/^.*href="//i' -e 's/\.zip.*$/.zip/')
+
+
+if [ -n "$LOCATION" ]
+then
+    RELEASE_URL="${BASEURL}$LOCATION"
+fi
+
+echo $RELEASE_URL
