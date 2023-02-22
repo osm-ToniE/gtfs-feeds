40a45aab249bb27db0caaa386e96f25d7bb81eb3
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-22 17:17:04 +0100

    add GTFS feed FR-PAC-TEDbus

---
+++ FR/PAC/TEDbus/get-release-url.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+echo "https://trouver.datasud.fr/dataset/f1d3c67a-3871-4a4b-93d8-794b60381d7f/resource/2bffb335-f293-4390-9dbd-f130d2c85319/download/tedbus.gtfs.zip"
