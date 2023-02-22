de20ee3387dd0315606ccfa11b8c4701581a21e7
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-22 17:41:05 +0100

    add GTFS feed FR-PAC-TransCoVe

---
+++ FR/PAC/TransCoVe/get-release-url.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+echo "https://trouver.datasud.fr/dataset/3560aa41-a61f-4f6a-a079-f6e788a4e3d0/resource/9dc7e6b7-01c3-4404-8862-de323b80dffd/download/gtfs.zip"
