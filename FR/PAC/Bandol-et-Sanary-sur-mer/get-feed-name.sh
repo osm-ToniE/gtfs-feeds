fc2c030b16974e09a6cb14d563023af8fa15953c
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-22 16:36:48 +0100

    add GTFS feed FR-PAC-Bandol-et-Sanary-sur-mer

---
+++ FR/PAC/Bandol-et-Sanary-sur-mer/get-feed-name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# print name of GTFS feed
+#
+
+echo "FR-PAC-Bandol-et-Sanary-sur-mer"
---
+++ FR/PAC/Bandol-et-Sanary-sur-mer/get-release-url.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+PERMALINK="https://www.data.gouv.fr/fr/datasets/r/358ab51c-78e8-45a4-ae5a-a05722af6c09"
+
+LOCATION=$(curl -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+if [ -n "$LOCATION" ]
+then
+    if [ "$(echo $LOCATION | grep -c 'bandol-sanary')" == 1 ]
+    then
+        RELEASE_URL=$LOCATION
+    fi
+fi
+
+echo $RELEASE_URL
