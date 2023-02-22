901b8c6719985728ffe41bbcc47caf8f86416fbc
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-22 17:06:07 +0100

    add GTFS feed FR-PAC-Mouvenbus

---
+++ FR/PAC/Mouvenbus/get-release-url.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+echo "https://trouver.datasud.fr/dataset/d0409db6-1dc5-4bca-b071-ff31e3fd6d56/resource/69babb86-0652-463a-9c39-2b762f54febf/download/mouvenbus.gtfs.zip"
