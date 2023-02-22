81c51c75c608d7c982ae911af0f2a2fe000f4502
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-22 17:40:49 +0100

    add GTFS feed FR-ARA-Montelibus

---
+++ FR/ARA/Montelibus/get-release-url.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+PERMALINK="https://www.data.gouv.fr/fr/datasets/r/da505f8a-0c65-4877-bc7a-fb21c584f6bd"
+
+LOCATION=$(curl -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+if [ -n "$LOCATION" ]
+then
+    if [ "$(echo $LOCATION | grep -c 'montelibus-montelimar')" == 1 ]
+    then
+        RELEASE_URL=$LOCATION
+    fi
+fi
+
+echo $RELEASE_URL
