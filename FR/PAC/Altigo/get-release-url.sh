c4bb46b88cadc79725781a5557d13d7eacbb4601
Toni Erdmann <Toni.Erdmann@web.de>
2023-02-22 16:51:57 +0100

    add GTFS feed FR-PAC-Altigo

---
+++ FR/PAC/Altigo/get-release-url.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+PERMALINK="https://www.data.gouv.fr/fr/datasets/r/b4c1ce7f-0729-44bd-8d47-cc982a15f745"
+
+LOCATION=$(curl -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+if [ -n "$LOCATION" ]
+then
+    if [ "$(echo $LOCATION | grep -c 'reseau-altigo')" == 1 ]
+    then
+        RELEASE_URL=$LOCATION
+    fi
+fi
+
+echo $RELEASE_URL
