b2456114fb70bc509422bcd47d738cb9b9e979d8
Toni Erdmann <Toni.Erdmann@web.de>
2023-09-26 14:37:25 +0200

    adapt to new location

--- FR/ARA/Montelibus/get-release-url.sh
+++ FR/ARA/Montelibus/get-release-url.sh
@@ -4,13 +4,13 @@
 # get URL to download latest GTFS feed
 #
 
-PERMALINK="https://www.data.gouv.fr/fr/datasets/r/da505f8a-0c65-4877-bc7a-fb21c584f6bd"
+PERMALINK="https://www.data.gouv.fr/fr/datasets/r/01c10bc3-da1a-4a3d-b72a-beadff6e9fef"
 
 LOCATION=$(curl --connect-timeout 30 -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
 
 if [ -n "$LOCATION" ]
 then
-    if [ "$(echo $LOCATION | grep -c 'montelibus-montelimar')" == 1 ]
+    if [ "$(echo $LOCATION | grep -c 'gtfs.zip')" == 1 ]
     then
         RELEASE_URL=$LOCATION
     fi
