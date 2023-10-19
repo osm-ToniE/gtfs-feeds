8a8806eae56a52242976a0597649a268c37ed069
Toni Erdmann <Toni.Erdmann@web.de>
2023-10-19 16:42:09 +0200

    adapt script to potential new format

--- DE/SN/VMS/get-release-date.sh
+++ DE/SN/VMS/get-release-date.sh
@@ -6,7 +6,7 @@
 
 SCANURL="https://www.vms.de/vms/service/downloads/"
 
-SCANDATE=$(curl --connect-timeout 30 -s $SCANURL -o - | egrep -i 'href="https://www.vms.de/wp-content/uploads/.*/GTFS_VMS_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]+\.zip"' | tail -1 | sed -e 's/^.*GTFS_VMS_//i' -e 's/\.zip.*$//')
+SCANDATE=$(curl --connect-timeout 30 -s $SCANURL -o - | egrep -i 'href="https://www.vms.de/wp-content/uploads/.*/GTFS_VMS_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9].*?\.zip"' | tail -1 | sed -e 's/^.*GTFS_VMS_//i' -e 's/\.zip.*$//' -e 's/-.$//')
 
 if [ -n "$SCANDATE" ]
 then
