b8ca7002137b907ac6652dc0820c7f684c20f78a
Toni Erdmann <Toni.Erdmann@web.de>
2023-11-26 13:33:02 +0100

    add GTFS feed for FR-CVL-TAO

---
+++ FR/CVL/TAO/get-release-url.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+PERMALINK="https://chouette.enroute.mobi/api/v1/datas/keolis_orleans.gtfs.zip"
+LOCATION=$(curl --connect-timeout 30 -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+if [ -n "$LOCATION" ]
+then
+    if [ "$(echo $LOCATION | grep -c 'gtfs.zip')" == 1 ]
+    then
+        RELEASE_URL=$LOCATION
+    fi
+fi
+
+echo $RELEASE_URL
