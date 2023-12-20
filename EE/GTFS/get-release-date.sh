caf9b23b20d4c052f9fd26814f9a1bc1c806f5c7
Toni Erdmann <Toni.Erdmann@web.de>
2023-12-20 11:53:10 +0100

    add GTFS support for Estonia

---
+++ EE/GTFS/get-release-date.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# retrieve release date of latest GTFS feed in form "YYYY-MM-DD"
+#
+
+RELEASE_URL=$(./get-release-url.sh)
+
+if [ -n "$RELEASE_URL" ]
+then
+    mkdir tempdir
+
+    wget -q -O tempdir/gtfs.zip --user-agent="PTNA @ ptna.openstreetmap.de" $RELEASE_URL
+
+    if [ -f tempdir/gtfs.zip -a -s tempdir/gtfs.zip ]
+    then
+        result=$(unzip -l tempdir/gtfs.zip | awk '/20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]/ { print $2; }' | sort -u | tail -1)
+        if [ "$(echo $result | grep -c '^20[0-9][0-9]-[01][0-9]-[0123][0-9]$')" == 1 ]
+        then
+            RELEASE_DATE=$result
+
+            mv tempdir $RELEASE_DATE
+        fi
+    fi
+fi
+
+echo $RELEASE_DATE
---
+++ EE/GTFS/get-release-url.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+echo "http://www.peatus.ee/gtfs/gtfs.zip"
