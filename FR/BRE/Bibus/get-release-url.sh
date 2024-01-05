3e2da3871d66888a48c5a818852f47f3831f37c8
Toni Erdmann <Toni.Erdmann@web.de>
2024-01-05 18:58:39 +0100

    add support for FR-BRE-bibus

---
+++ FR/BRE/Bibus/get-release-url.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+RELEASE_URL="https://ratpdev-mosaic-prod-bucket-raw.s3-eu-west-1.amazonaws.com/11/exports/1/gtfs.zip"
+
+echo $RELEASE_URL
