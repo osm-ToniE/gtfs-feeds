e2f39d6b0edcbc29b5aa9038bf7830b60dad4d29
Toni Erdmann <Toni.Erdmann@web.de>
2024-01-08 15:28:53 +0100

    set alteernative download url
    - original one not reachable from "everest" server (Firewall?)

--- DE/BY/VGN/get-release-url.sh
+++ DE/BY/VGN/get-release-url.sh
@@ -4,4 +4,6 @@
 # get URL to download latest GTFS feed
 #
 
-echo "https://www.vgn.de/opendata/GTFS.zip"
+# echo "https://www.vgn.de/opendata/GTFS.zip"
+
+echo "https://gtfs.mfdz.de/VGN.raw.gtfs.zip"
