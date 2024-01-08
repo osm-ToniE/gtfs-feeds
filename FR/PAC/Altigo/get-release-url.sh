36a55f76b51d8e8d8fcd6e526345eef863e6be23
Toni Erdmann <Toni.Erdmann@web.de>
2024-01-08 18:19:38 +0100

    fix release_url

--- FR/PAC/Altigo/get-release-url.sh
+++ FR/PAC/Altigo/get-release-url.sh
@@ -4,16 +4,17 @@
 # get URL to download latest GTFS feed
 #
 
-PERMALINK="https://www.data.gouv.fr/fr/datasets/r/35cf4d6c-a45b-442f-9cd0-a3293d344a9d"
+DATASET_ID="63c562c646696c9cd83544b7"
 
-LOCATION=$(curl --connect-timeout 30 -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
+
+LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o - | \
+         jq '.resources[0] | .original_url'           | \
+         sed -e 's/"//g')
 
 if [ -n "$LOCATION" ]
 then
-    if [ "$(echo $LOCATION | grep -c 'reseau-altigo')" == 1 ]
-    then
-        RELEASE_URL=$LOCATION
-    fi
+    RELEASE_URL=$LOCATION
 fi
 
 echo $RELEASE_URL
