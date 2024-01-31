5326a8bd155cac70737dd06fbcc22565b4e8df0f
Toni Erdmann <Toni.Erdmann@web.de>
2024-01-31 17:47:13 +0100

    update release url / location

--- FR/BRE/Bibus/get-release-url.sh
+++ FR/BRE/Bibus/get-release-url.sh
@@ -8,9 +8,11 @@ DATASET_ID="55ffbe0888ee387348ccb97d"
 
 JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
 
-LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o - | \
-         jq '.resources[0] | .original_url'           | \
-         sed -e 's/"//g')
+LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o -                                     | \
+         jq -r '.resources[] | select(.format=="GTFS") | (.updated + "_" + .original_url)' | \
+         sort                                                                              | \
+         tail -1                                                                           | \
+         sed -e 's/^.*Z_http/http/')
 
 if [ -n "$LOCATION" ]
 then
