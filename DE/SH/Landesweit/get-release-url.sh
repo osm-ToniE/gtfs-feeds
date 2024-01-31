ba46162d8bba896eab272983f9e6fba023e1a2e6
Toni Erdmann <Toni.Erdmann@web.de>
2024-01-31 17:38:07 +0100

    fix location of data

--- DE/SH/Landesweit/get-release-url.sh
+++ DE/SH/Landesweit/get-release-url.sh
@@ -7,8 +7,10 @@
 SCANURL="https://opendata.schleswig-holstein.de/dataset/fahrplandaten"
 BASEURL="https://opendata.schleswig-holstein.de"
 
-LOCATION=$(curl --connect-timeout 30 -s $SCANURL -o - | egrep -i 'href="/dataset/.*/download/fahrplandaten.zip"' | tail -1 | sed -e 's/^.*href="//i' -e 's/\.zip.*$/.zip/')
-
+LOCATION=$(curl --connect-timeout 30 -s $SCANURL -o - | \
+           egrep -i 'href="/dataset/.*/resource/777b9b5b-f24f-491b-a19b-e01bb5704bd2/download/fahrplandaten"' | \
+           tail -1 | \
+           sed -e 's/^.*href="//i' -e 's/fahrplandaten.*$/fahrplandaten/')
 
 if [ -n "$LOCATION" ]
 then
