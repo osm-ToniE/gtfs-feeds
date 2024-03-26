0c2d767612a0c5ad2d91220117759fff82c2c22c
Toni Erdmann <Toni.Erdmann@web.de>
2024-03-26 12:23:13 +0100

    remove "suspicious" entry from N71
    - starts with 2 * Westfriedhof

--- DE/BY/MVG/post-process-ptna-sqlite.sh
+++ DE/BY/MVG/post-process-ptna-sqlite.sh
@@ -11,3 +11,7 @@ sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_number_of_sto
 # Bus 99 has only 2 Stops, that's OK
 
 sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_number_of_stops='' WHERE trip_id LIKE '%-99-%';"
+
+# Bus N71 starts in GTFS with 2 times "Westfriedhof", that's OK, so deletion of comment in DB
+
+sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_start='' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-N71-%' AND stop_name='Westfriedhof' AND stop_sequence=1);"
