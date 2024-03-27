2076749dd0ede217d6a3b912e3fccf08ac0eb7a9
Toni Erdmann <Toni.Erdmann@web.de>
2024-03-27 16:44:34 +0100

    O7 (internal number 407) has only 2 stops: OK

--- DE/BY/MVG/post-process-ptna-sqlite.sh
+++ DE/BY/MVG/post-process-ptna-sqlite.sh
@@ -4,9 +4,14 @@ DB="ptna-gtfs-sqlite.db"
 
 SQ_OPTIONS="-init /dev/null"
 
-# Bus O7 has only 2 Stops, that's OK
+# Bus O7 (internally 407) has only 2 Stops, that's OK - usually 407 would be a regional bus, but this is MVG, not MVV feed
 
 sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_number_of_stops='' WHERE trip_id LIKE '%-O7-%';"
+sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_number_of_stops='' WHERE trip_id LIKE '%-407-%';"
+
+# Bus 97 has only 2 Stops, that's OK
+
+sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_number_of_stops='' WHERE trip_id LIKE '%-97-%';"
 
 # Bus 99 has only 2 Stops, that's OK
 
