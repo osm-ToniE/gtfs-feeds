d1a4bdf28a8706e8de4895c7e6770a8f38b9ddec
Toni Erdmann <Toni.Erdmann@web.de>
2024-03-30 19:56:51 +0100

    add support for BG-22-Sofia GTFS

---
+++ BG/22/Sofia/get-feed-name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# print name of GTFS feed
+#
+
+echo "BG-22-Sofia"
---
+++ BG/22/Sofia/get-release-url.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+PERMALINK="https://github.com/Dimitar5555/sofiatraffic-gtfs/raw/master/result.zip"
+
+LOCATION=$(curl --connect-timeout 30 -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+if [ -n "$LOCATION" ]
+then
+    if [ "$(echo $LOCATION | grep -c 'Dimitar5555')" == 1 ]
+    then
+        RELEASE_URL=$LOCATION
+    fi
+fi
+
+echo $RELEASE_URL
