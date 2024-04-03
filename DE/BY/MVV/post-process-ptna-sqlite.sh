6de425117eaaf30e8acc00c5a207172ace7272f2
Toni Erdmann <Toni.Erdmann@web.de>
2024-04-03 21:12:54 +0200

    delete hint for suspicious end for X204

--- DE/BY/MVV/post-process-ptna-sqlite.sh
+++ DE/BY/MVV/post-process-ptna-sqlite.sh
@@ -4,6 +4,10 @@ DB="ptna-gtfs-sqlite.db"
 
 SQ_OPTIONS="-init /dev/null"
 
+# Bus X204 ends in GTFS with 2 times "St. Achaz", that's OK, so deletion of comment in DB
+
+sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_end='' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-204-%' AND stop_name='St. Achaz' AND stop_sequence=14);"
+
 # Bus 222 starts in GTFS with 2 times "Deisenhofen", that's OK, so deletion of comment in DB
 
 sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_start='' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-222-%' AND stop_name='Deisenhofen' AND stop_sequence=1);"
