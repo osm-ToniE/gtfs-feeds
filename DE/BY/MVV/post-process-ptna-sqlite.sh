b460ee078612e0d405bdb5d5bfdb93ed5183aefb
Toni Erdmann <Toni.Erdmann@web.de>
2024-04-17 16:18:56 +0200

    adadt to changes in GTFS feed
    - "Deisenhofen" has been renamed to "Deisenhofen West"

--- DE/BY/MVV/post-process-ptna-sqlite.sh
+++ DE/BY/MVV/post-process-ptna-sqlite.sh
@@ -8,6 +8,6 @@ SQ_OPTIONS="-init /dev/null"
 
 sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_end='' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-204-%' AND stop_name='St. Achaz' AND stop_sequence=14);"
 
-# Bus 222 starts in GTFS with 2 times "Deisenhofen", that's OK, so deletion of comment in DB
+# Bus 222 starts in GTFS with 2 times "Deisenhofen West", that's OK, so deletion of comment in DB
 
-sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_start='' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-222-%' AND stop_name='Deisenhofen' AND stop_sequence=1);"
+sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_start='' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-222-%' AND stop_name='Deisenhofen West' AND stop_sequence=1);"
