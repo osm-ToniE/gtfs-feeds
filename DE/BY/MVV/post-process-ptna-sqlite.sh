b846c945ec13cc886ad10f163c999bef6a2ce667
Toni Erdmann <Toni.Erdmann@web.de>
2024-04-25 18:57:50 +0200

    set "suspicious_other": some trips can not be found in the printed/online time tables

--- DE/BY/MVV/post-process-ptna-sqlite.sh
+++ DE/BY/MVV/post-process-ptna-sqlite.sh
@@ -15,3 +15,11 @@ sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_number_of_sto
 # Bus 222 starts in GTFS with 2 times "Deisenhofen West", that's OK, so deletion of comment in DB
 
 sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_start='' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-222-%' AND stop_name='Deisenhofen West' AND stop_sequence=1);"
+
+# Bus 903 is in GTFS but not in PDF/WEB time tables
+
+for trip in $(sqlite3 $SQ_OPTIONS $DB "SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-903-%' AND stop_name='Söcking, Auersberg' AND stop_sequence=3;")
+do
+    sqlite3 $SQ_OPTIONS $DB "INSERT OR IGNORE INTO ptna_trips_comments (trip_id,suspicious_other) VALUES ('$trip','Nicht im Online-Fahrplan enthalten');"
+done
+sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_other='Nicht im Online-Fahrplan enthalten' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-903-%' AND stop_name='Söcking, Auersberg' AND stop_sequence=3);"
