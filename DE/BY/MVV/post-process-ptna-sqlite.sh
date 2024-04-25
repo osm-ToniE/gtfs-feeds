2461598bab95f59d5a85e93bb10ac2f4f873adde
Toni Erdmann <Toni.Erdmann@web.de>
2024-04-25 12:49:36 +0200

    delete trip comment: Bus X732 has 2 trips with 2 stops only: Odelzhausen to Dasing 05:02 and 06:02 in GTFS

--- DE/BY/MVV/post-process-ptna-sqlite.sh
+++ DE/BY/MVV/post-process-ptna-sqlite.sh
@@ -8,6 +8,10 @@ SQ_OPTIONS="-init /dev/null"
 
 sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_end='' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-204-%' AND stop_name='St. Achaz' AND stop_sequence=14);"
 
+# Bus X732 has 2 trips with 2 stops only: Odelzhausen to Dasing 05:02 and 06:02 in GTFS, so deletion of comment in DB
+
+sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_number_of_stops='' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%20-732-%' AND stop_name='Dasing, Bahnhof' AND stop_sequence=2);"
+
 # Bus 222 starts in GTFS with 2 times "Deisenhofen West", that's OK, so deletion of comment in DB
 
 sqlite3 $SQ_OPTIONS $DB "UPDATE ptna_trips_comments SET suspicious_start='' WHERE trip_id IN (SELECT trip_id FROM stop_times JOIN stops ON stops.stop_id=stop_times.stop_id WHERE trip_id LIKE '%-222-%' AND stop_name='Deisenhofen West' AND stop_sequence=1);"
