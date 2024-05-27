92e1df86ed34045870a9d1db259ec53b772cf37d
Toni Erdmann <Toni.Erdmann@web.de>
2024-05-27 13:00:06 +0200

    add network "FR-BRE-TILT"

---
+++ FR/BRE/TILT/get-release-url.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+DATASET_ID="66512dc2e8c9915204e5a98b"
+
+JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
+
+LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o -                                     | \
+         jq -r '.resources[] | select(.format=="GTFS") | (.updated + "_" + .original_url)' | \
+         sort                                                                              | \
+         tail -1                                                                           | \
+         sed -e 's/^.*Z_http/http/')
+
+if [ -n "$LOCATION" ]
+then
+    RELEASE_URL=$LOCATION
+fi
+
+echo $RELEASE_URL
