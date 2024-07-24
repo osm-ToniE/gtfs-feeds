06497566fbe4d195bd30668021814cef16c9089b
Toni Erdmann <Toni.Erdmann@web.de>
2024-07-24 22:31:25 +0200

    add 'network' FR-ARA-SMTCAC

---
+++ FR/ARA/SMTCAC/get-release-url.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+PERMALINK="https://opendata.clermontmetropole.eu/api/v2/catalog/datasets/gtfs-smtc/attachments/gtfs_t2c_plus_scolaire_zip"
+
+echo $PERMALINK
