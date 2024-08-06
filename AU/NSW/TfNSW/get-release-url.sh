e3c63915ce718c35bd44f793af9fd12d04035cd6
Toni Erdmann <Toni.Erdmann@web.de>
2024-08-06 13:05:58 +0200

    add GTFS data provided by "Transport for New South Wales", Australia

---
+++ AU/NSW/TfNSW/get-release-url.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+echo "https://opendata.transport.nsw.gov.au/dataset/d1f68d4f-b778-44df-9823-cf2fa922e47f/resource/67974f14-01bf-47b7-bfa5-c7f2f8a950ca/download/full_greater_sydney_gtfs_static_0.zip"
