aba8cd5ea164e464309d571b168a7fa4ad53928b
Toni Erdmann <Toni.Erdmann@web.de>
2024-08-20 17:35:46 +0200

    adapt to new location of resource

--- FR/ARA/Montelibus/get-release-url.sh
+++ FR/ARA/Montelibus/get-release-url.sh
@@ -4,7 +4,7 @@
 # get URL to download latest GTFS feed
 #
 
-PERMALINK="https://www.data.gouv.fr/fr/datasets/r/01c10bc3-da1a-4a3d-b72a-beadff6e9fef"
+PERMALINK="https://www.data.gouv.fr/fr/datasets/r/3c868712-8920-4ec8-a691-ff6876ecb29e"
 
 LOCATION=$(curl --connect-timeout 30 -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
 
