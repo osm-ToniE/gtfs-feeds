d93876b1086e46793212660e1ab2d41fc9100981
Toni Erdmann <Toni.Erdmann@web.de>
2024-09-13 15:06:29 +0200

    add analysis for Israel

---
+++ IL/MOT/get-release-date.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# retrieve release date of latest GTFS feed in form "YYYY-MM-DD"
+#
+
+RELEASE_URL=$(./get-release-url.sh)
+
+if [ -n "$RELEASE_URL" ]
+then
+    RELEASE_DIR=${RELEASE_URL%/*}
+    RELEASE_FILE=${RELEASE_URL##*/}
+    LAST_MODIFIED=$(wget --no-check-certificate -O - $RELEASE_DIR 2>&1  | \
+                    sed -e 's/<br>/\n/ig'                                   | \
+                    fgrep "$RELEASE_FILE"                                   | \
+                    sed -e 's/^\s*//' -e 's/\s.*$//')
+
+    if [ -n "$LAST_MODIFIED" ]
+    then
+        result=$(date -d "$LAST_MODIFIED" '+%Y-%m-%d')
+        if [ "$(echo $result | grep -c '^20[0-9][0-9]-[01][0-9]-[0123][0-9]$')" == 1 ]
+        then
+            RELEASE_DATE=$result
+        fi
+    fi
+fi
+
+echo $RELEASE_DATE
