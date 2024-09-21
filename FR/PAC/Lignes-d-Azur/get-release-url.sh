756b2db96e893b70e099c8236a343255083e512b
Toni Erdmann <Toni.Erdmann@web.de>
2024-09-21 14:57:12 +0200

    adapt to new URL name

--- FR/PAC/Lignes-d-Azur/get-release-url.sh
+++ FR/PAC/Lignes-d-Azur/get-release-url.sh
@@ -4,21 +4,21 @@
 # get URL to download latest GTFS feed
 #
 
-PERMALINK="https://opendata.nicecotedazur.org/data/dataset/export-quotidien-au-format-gtfs-du-reseau-de-transport-lignes-d-azur/resource/aacb4eea-d008-4b13-b17a-848b8ced7e03/download"
+DATASET_ID="59aea7b3b595087a239169d7"
 
-LOCATION=$(curl --connect-timeout 30 -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
+
+LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o - | \
+         jq '.resources[0] | .original_url'           | \
+         sed -e 's/"//g')
 
 if [ -n "$LOCATION" ]
 then
-    if [ "$(echo $LOCATION | grep -c 'http://')" == 1 ]
+    LOCATION=$(curl --connect-timeout 30 -sI $LOCATION | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+    if [ -n "$LOCATION" ]
     then
-        OTHER_LOCATION=$(curl --connect-timeout 30 -sI $LOCATION | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
-        if [ "$(echo $OTHER_LOCATION | grep -c '//opendata.nicecotedazur.org/')" == 1 ]
-        then
-            RELEASE_URL=$OTHER_LOCATION
-        fi
-    else
-        if [ "$(echo $LOCATION | grep -c '//opendata.nicecotedazur.org/')" == 1 ]
+        if [ "$(echo $LOCATION | grep -c 'nicecotedazur')" == 1 ]
         then
             RELEASE_URL=$LOCATION
         fi
