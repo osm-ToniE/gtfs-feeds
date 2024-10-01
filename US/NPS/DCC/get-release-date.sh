b0be2c3e7ec4b7f3996e0dce9c31f017d64d586d
Toni Erdmann <Toni.Erdmann@web.de>
2024-10-01 19:14:37 +0200

    report RELEASE_DATE only if HTTP response code is 200

--- US/NPS/DCC/get-release-date.sh
+++ US/NPS/DCC/get-release-date.sh
@@ -8,13 +8,13 @@ RELEASE_URL=$(./get-release-url.sh)
 
 if [ -n "$RELEASE_URL" ]
 then
-    LAST_MODIFIED=$(curl --connect-timeout 30 -sI $RELEASE_URL | egrep -i '^(HTTP/. 404|last-modified:)' | sed -e 's/^last-modified:\s*//i')
+    LAST_MODIFIED=$(curl --connect-timeout 30 -sI $RELEASE_URL | egrep -i '^(HTTP/. 404|last-modified:)')
 
     if [ -n "$LAST_MODIFIED" ]
     then
-        if [ $(echo $LAST_MODIFIED | wc -l) -eq 2 ]
+        if [ $(echo $LAST_MODIFIED |fgrep -c 'HTTP/2 200') -eq 1 ]
         then
-            LAST_MODIFIED=$(echo $LAST_MODIFIED | tail -1)
+            LAST_MODIFIED=$(echo $LAST_MODIFIED | sed -e 's/.*last-modified:\s*//i')
             result=$(date -d "$LAST_MODIFIED" '+%Y-%m-%d')
             if [ "$(echo $result | grep -c '^20[0-9][0-9]-[01][0-9]-[0123][0-9]$')" == 1 ]
             then
