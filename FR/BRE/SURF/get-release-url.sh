41476feeb09f939728ce5600b3ffb580b8d53245
Toni Erdmann <Toni.Erdmann@web.de>
2024-11-26 14:12:07 +0100

    add GTFS feed for FR-BRE-SURF

---
+++ FR/BRE/SURF/get-release-url.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+DATASET_ID="588a238d88ee3846659b81a4"
+
+JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
+
+LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o -                                     | \
+         jq -r '.resources[] | select(.format=="GTFS") | (.updated + "_" + .original_url)' | \
+         sort                                                                              | \
+         tail -1                                                                           | \
+         sed -e 's/^.*Z_http/http/')
+
+if [ -n "$LOCATION" ]
+then
+    RELEASE_URL=$LOCATION
+fi
+
+echo $RELEASE_URL
