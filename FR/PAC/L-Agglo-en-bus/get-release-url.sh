84a5b621d356452365ed073e1e962165eda61fbd
Toni Erdmann <Toni.Erdmann@web.de>
2024-12-25 19:57:11 +0100

    add GTFS data for FR-PAC-L-Agglo-en-bus

--- FR/PAC/L-Agglo-en-bus/get-release-url.sh
+++ FR/PAC/L-Agglo-en-bus/get-release-url.sh
@@ -8,15 +8,20 @@ DATASET_ID="65a7d7a7fbf0d8d41d572b1b"
 
 JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
 
-LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o -                                     | \
+ORIGINAL_URL=$(curl --connect-timeout 30 -s $JSON_URL -o -                                     | \
          jq -r '.resources[] | select(.format=="GTFS") | (.updated + "_" + .original_url)' | \
          sort                                                                              | \
          tail -1                                                                           | \
          sed -e 's/^.*Z_http/http/')
 
-if [ -n "$LOCATION" ]
+if [ -n "$ORIGINAL_URL" ]
 then
-    RELEASE_URL=$LOCATION
+    LOCATION=$(curl --connect-timeout 30 -sI $ORIGINAL_URL | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+    if [ -n "$LOCATION" ]
+    then
+        RELEASE_URL=${ORIGINAL_URL%%.fr/*}.fr/$LOCATION
+    fi
 fi
 
 echo $RELEASE_URL
