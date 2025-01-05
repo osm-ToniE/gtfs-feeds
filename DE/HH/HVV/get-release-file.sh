6ce5f0a9c1e2dc4f8eb16ea8855fa7babc524d22
Toni Erdmann <Toni.Erdmann@web.de>
2025-01-05 13:37:52 +0100

    do not check certificate for DE-HH-HVV GTFS data

---
+++ DE/HH/HVV/get-release-file.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#
+# download latest GTFS feed data
+#
+
+RELEASE_URL=$(./get-release-url.sh)
+
+if [ -n "$RELEASE_URL" ]
+then
+    wget --no-check-certificate -O - "$RELEASE_URL"
+fi
