46efc2f23982e3b2dd93250fea12255b4a6112c0
Toni Erdmann <Toni.Erdmann@web.de>
2025-01-08 17:30:54 +0100

    add wget option --no-verbose

--- IL/MOT/get-release-file.sh
+++ IL/MOT/get-release-file.sh
@@ -8,5 +8,5 @@ RELEASE_URL=$(./get-release-url.sh)
 
 if [ -n "$RELEASE_URL" ]
 then
-    wget --no-check-certificate -O - "$RELEASE_URL"
+    wget --no-check-certificate --no-verbose --user-agent "PTNA script on https://ptna.openstreetmap.de" -O - "$RELEASE_URL"
 fi
--- SE/Samtrafiken/get-release-file.sh
+++ SE/Samtrafiken/get-release-file.sh
@@ -8,7 +8,7 @@ RELEASE_URL=$(./get-release-url.sh)
 
 if [ -n "$RELEASE_URL" ]
 then
-    wget --user-agent "PTNA script on https://ptna.openstreetmap.de" --header "Accept-Encoding: gzip" -O - "$RELEASE_URL$SE_SAMTRAFIKEN_APIKEY"
+    wget --no-verbose --user-agent "PTNA script on https://ptna.openstreetmap.de" --header "Accept-Encoding: gzip" -O - "$RELEASE_URL$SE_SAMTRAFIKEN_APIKEY"
 else
     echo "$(./get-feed-name.sh): could not evaluate release URL" >> /dev/stderr
     echo ""
