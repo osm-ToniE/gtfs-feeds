b2efcf8cf352d506d2d73a753b1eebc6c9634b6a
Toni Erdmann <Toni.Erdmann@web.de>
2025-01-09 17:44:14 +0100

    be a bit more verbose with download_dataset()

--- AT/OOeVV/get-release-file.sh
+++ AT/OOeVV/get-release-file.sh
@@ -18,6 +18,7 @@ then
 
     if [ "$(echo $year | grep -c '^20[0-9][0-9]$')" == 1 ]
     then
+        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "download_dataset token $dsid $year"
         download_dataset $token $dsid $year
     else
         echo "$PWD: could not get correct information for 'year' ($year)" >> /dev/stderr
