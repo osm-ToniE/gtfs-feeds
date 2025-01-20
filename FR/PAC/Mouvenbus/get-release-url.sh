1ca0c5041500546254703e3114b208d5f3ef646b
Toni Erdmann <Toni.Erdmann@web.de>
2025-01-20 21:38:26 +0100

    correctly find GTFS resource
    - it is not resources[0]

--- FR/PAC/Mouvenbus/get-release-url.sh
+++ FR/PAC/Mouvenbus/get-release-url.sh
@@ -8,13 +8,15 @@ DATASET_ID="66da461b80f91630d0227702"
 
 JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
 
-LOCATION="$(curl --connect-timeout 30 -s $JSON_URL -o - | \
-         jq '.resources[0] | .original_url'           | \
-         sed -e 's/"//g' -e 's/\/$//')/"
+ORIGINAL_URL=$(curl --connect-timeout 30 -s $JSON_URL -o -                                     | \
+         jq -r '.resources[] | select(.format=="GTFS") | (.updated + "_" + .original_url)' | \
+         sort                                                                              | \
+         tail -1                                                                           | \
+         sed -e 's/^.*Z_http/http/')
 
-if [ -n "$LOCATION" ]
+if [ -n "$ORIGINAL_URL" ]
 then
-    RELEASE_URL=$LOCATION
+    RELEASE_URL=$ORIGINAL_URL
 fi
 
 echo $RELEASE_URL
