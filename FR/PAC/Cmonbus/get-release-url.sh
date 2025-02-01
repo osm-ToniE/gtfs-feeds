c266cca06f89ab6d9a827441b6a527a874c5ab3d
Patchi-Osm <76591095+Patchi-Osm@users.noreply.github.com>
2025-02-01 09:06:22 +0100

    Create Cmonbus 4/6
    Fourth file

---
+++ FR/PAC/Cmonbus/get-release-url.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+DATASET_ID="6582c0551499a457530f4c93"
+
+JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
+
+ORIGINAL_URL=$(curl --connect-timeout 30 -s $JSON_URL -o -                                     | \
+         jq -r '.resources[] | select(.format=="GTFS") | (.updated + "_" + .original_url)' | \
+         sort                                                                              | \
+         tail -1                                                                           | \
+         sed -e 's/^.*Z_http/http/')
+
+if [ -n "$ORIGINAL_URL" ]
+then
+    LOCATION=$(curl --connect-timeout 30 -sI $ORIGINAL_URL | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+    if [ -n "$LOCATION" ]
+    then
+        RELEASE_URL=${ORIGINAL_URL%%.fr/*}.fr/$LOCATION
+    fi
+fi
+
+echo $RELEASE_URL
