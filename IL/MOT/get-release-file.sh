d2572a2fd32f162bd5e8f9a74af2367227d0d656
Toni Erdmann <Toni.Erdmann@web.de>
2025-02-03 21:35:31 +0100

    fix missing double quotes

--- IL/MOT/get-release-file.sh
+++ IL/MOT/get-release-file.sh
@@ -8,7 +8,7 @@ RELEASE_URL=$(./get-release-url.sh)
 
 if [ -n "$RELEASE_URL" ]
 then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') Download GTFS data from '$RELEASE_URL' >> /dev/stderr
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Download GTFS data from '$RELEASE_URL'" >> /dev/stderr
     # wget --no-check-certificate --no-verbose --user-agent "PTNA script on https://ptna.openstreetmap.de" -O - "$RELEASE_URL"
     wget --no-check-certificate --no-verbose -O - "$RELEASE_URL"
 fi
