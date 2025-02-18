7d2ec3573a785a7a48591afd317ad0f4fd65582b
Toni Erdmann <Toni.Erdmann@web.de>
2025-02-18 16:04:13 +0100

    fix URL

--- FR/ARA/SMTCAC/get-release-url.sh
+++ FR/ARA/SMTCAC/get-release-url.sh
@@ -4,6 +4,7 @@
 # get URL to download latest GTFS feed
 #
 
-PERMALINK="https://opendata.clermontmetropole.eu/api/datasets/1.0/gtfs-smtc/attachments/gtfs_t2c_plus_scolaire_zip/"
+#PERMALINK="https://opendata.clermontmetropole.eu/api/datasets/1.0/gtfs-smtc/attachments/gtfs_t2c_plus_scolaire_zip/"
+PERMALINK="https://opendata.clermontmetropole.eu/api/explore/v2.1/catalog/datasets/gtfs-smtc/alternative_exports/gtfs"
 
 echo $PERMALINK
