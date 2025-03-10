ec5cd6192d4b13569bf8e746100c5f6068156cc6
Toni Erdmann <Toni.Erdmann@web.de>
2025-03-10 16:27:12 +0100

    improve error handling in case of rejected connection

--- FR/BRE/TILT/get-release-date.sh
+++ FR/BRE/TILT/get-release-date.sh
@@ -20,10 +20,20 @@ then
     else
         curl --connect-timeout 30 -sI -v $RELEASE_URL 2>&1 | grep -E '(^HTTP/)|(SSL certificate problem)' > ./release_date_error.log
 
-        if [ ! -s ./release_date_error.log ]
+        if [ -s ./release_date_error.log ]
         then
-            echo "Connection denied by Geolocation Setting. Reason: Blocked country: Finland" > ./release_date_error.log
-        fi
+            if [ $(grep -c 'SSL certificate problem' ./release_date_error.log) -eq 1 ]
+            then
+                curl -k --connect-timeout 30 -sI $RELEASE_URL 2>&1 | grep -i 'Content-Type' > ./release_date_error.log
+
+                if [ $(grep -i -c 'Content-Type: text/html' ./release_date_error.log) -eq 1 ]
+                then
+                    curl -k --connect-timeout 30 -v $RELEASE_URL 2>&1 | \
+                    grep '<title>'                                    | \
+                    sed -e 's/^.*<title>//' -e 's/<\/title>.*$//'     > ./release_date_error.log
+                fi
+            fi
+       fi
     fi
 fi
 
