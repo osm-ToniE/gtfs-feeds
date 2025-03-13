d86881bdc02a45f1f750e282a82601f12fa581a9
Toni Erdmann <Toni.Erdmann@web.de>
2025-03-13 01:08:26 +0100

    adapt to new file name

--- CL/RM/DTPM/get-release-url.sh
+++ CL/RM/DTPM/get-release-url.sh
@@ -7,11 +7,11 @@
 BASEURL="https://www.dtpm.cl"
 SCANURL="${BASEURL}/index.php/noticias/gtfs-vigente"
 
-# https://www.dtpm.cl/descargas/gtfs/GTFS_20250111.zip
+# https://www.dtpm.cl/descargas/gtfs/03%20GTFS_Final_03marzo.zip
 
 LOCATION=$(curl --connect-timeout 30 -s $SCANURL -o - | \
            sed -e 's/<\/a>/<\/a>\n/g' -e 's/<a>/<a>\n/g'  | \
-           egrep -i 'href="/descargas/gtfs/GTFS.*?\.zip">GTFS' | \
+           egrep -i 'href="/descargas/gtfs/.*?GTFS.*?\.zip">GTFS' | \
            head -1               | \
            sed -e 's/^.*href="//i' \
                -e 's/".*$//')
