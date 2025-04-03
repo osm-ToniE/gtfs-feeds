2cdf30d517e69b69693d3f87a268dffe3ba617ca
Toni Erdmann <Toni.Erdmann@web.de>
2025-04-03 12:50:36 +0200

    usae gtfs.mfdz.de/... as proxy for SWISS GTFS data

--- CH/Alle/get-release-url.sh
+++ CH/Alle/get-release-url.sh
@@ -4,16 +4,18 @@
 # get URL to download latest GTFS feed
 #
 
-PERMALINK="https://data.opentransportdata.swiss/de/dataset/timetable-2025-gtfs2020/permalink"
+echo "https://gtfs.mfdz.de/SWISS.raw.gtfs.zip"
 
-LOCATION=$(curl --connect-timeout 30 -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
-
-if [ -n "$LOCATION" ]
-then
-    if [ "$(echo $LOCATION | grep -c '^https://data.opentransportdata.swiss/')" == 1 ]
-    then
-        RELEASE_URL=$LOCATION
-    fi
-fi
-
-echo $RELEASE_URL
+#PERMALINK="https://data.opentransportdata.swiss/de/dataset/timetable-2025-gtfs2020/permalink"
+#
+#LOCATION=$(curl --connect-timeout 30 -sI $PERMALINK | fgrep -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+#
+#if [ -n "$LOCATION" ]
+#then
+#    if [ "$(echo $LOCATION | grep -c '^https://data.opentransportdata.swiss/')" == 1 ]
+#    then
+#        RELEASE_URL=$LOCATION
+#    fi
+#fi
+#
+#echo $RELEASE_URL
