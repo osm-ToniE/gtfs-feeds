d3583f18cc95860d1661cf49c28ecc6dc9c8af1f
Toni Erdmann <Toni.Erdmann@web.de>
2025-07-15 22:22:44 +0200

    add GTFS feed FR-BRE-ARBUS

---
+++ FR/BRE/ARBUS/get-release-file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# download latest GTFS feed data
+#
+
+RELEASE_URL=$(./get-release-url.sh)
+
+if [ -n "$RELEASE_URL" ]
+then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Download GTFS data from '$RELEASE_URL'" 1>&2
+    wget --no-verbose --no-check-certificate -O - "$RELEASE_URL"
+fi
---
+++ FR/BRE/ARBUS/get-release-url.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+RELEASE_URL=https://www.korrigo.bzh/ftp/OPENDATA/ARBUS.gtfs.zip
+
+echo $RELEASE_URL
