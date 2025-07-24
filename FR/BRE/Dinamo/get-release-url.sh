815b4b192044abdb122e181e9856336ba62958dc
Toni Erdmann <Toni.Erdmann@web.de>
2025-07-24 10:43:52 +0200

    add network/GTFS feed FR-BRE-Dinamo

---
+++ FR/BRE/Dinamo/get-release-url.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+DATASET_ID="60e324396e9514171898339c"
+
+JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
+
+LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o -                  | \
+         jq -r '.resources[] | select(.format=="GTFS") | .original_url' | \
+         sort                                                           | \
+         tail -1)
+
+if [ -n "$LOCATION" ]
+then
+    LAST_MODIFIED=$(curl --connect-timeout 30 -sI $LOCATION | grep -F -i 'last-modified:' | sed -e 's/^last-modified:\s*//i')
+
+    if [ -n "$LAST_MODIFIED" ]
+    then
+        RELEASE_URL=$LOCATION
+    else
+        LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o -                             | \
+                 jq -r '.history[] | .payload | select(.format=="GTFS") | .permanent_url'  | \
+                 sort                                                                      | \
+                 tail -1)
+        if [ -n "$LOCATION" ]
+        then
+            RELEASE_URL=$LOCATION
+        fi
+    fi
+fi
+
+echo $RELEASE_URL
