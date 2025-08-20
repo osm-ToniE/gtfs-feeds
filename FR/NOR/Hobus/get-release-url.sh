52f510c2df2446285bf215920eff549e36bb545e
Toni Erdmann <Toni.Erdmann@web.de>
2025-08-20 11:02:45 +0200

    add GTFS feed data FR-NOR-Hobus

---
+++ FR/NOR/Hobus/get-release-url.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+DATASET_ID="6780d441ff240aee47bd194f"
+
+JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
+
+LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o -                                     | \
+         jq -r '.resources[] | select(.format=="GTFS") | (.updated + "_" + .original_url)' | \
+         grep -F 'static'                                                                  | \
+         sort                                                                              | \
+         tail -1                                                                           | \
+         sed -e 's/^.*Z_http/http/')
+
+if [ $(echo $LOCATION | grep -c '^http.*static') -eq 1 ]
+then
+    RELEASE_URL=$LOCATION
+fi
+
+echo $RELEASE_URL
