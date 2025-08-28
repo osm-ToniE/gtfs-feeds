e2e7f42e7c24d4004dd31c3f4c6123ce96dcfe55
Toni Erdmann <Toni.Erdmann@web.de>
2025-08-28 15:36:06 +0200

    add GTFS feed for FR-PDL-Aleop_44

---
+++ FR/PDL/Aleop_44/get-feed-name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# print name of GTFS feed
+#
+
+echo "FR-PDL-Aleop_44"
---
+++ FR/PDL/Aleop_44/get-release-url.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+echo "https://donnees.paysdelaloire.fr/data/pdl44.zip"
