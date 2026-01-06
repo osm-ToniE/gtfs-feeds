6da7ba00c7c16ded4d2e7815737bc731b915d4f6
Toni Erdmann <Toni.Erdmann@web.de>
2026-01-06 18:39:14 +0100

    switch to 'gesamt' GTFS including MVG part but w/o shapes

--- DE/BY/MVV/get-release-url.sh
+++ DE/BY/MVV/get-release-url.sh
@@ -4,18 +4,6 @@
 # get URL to download latest GTFS feed
 #
 
-BASEURL="https://www.mvv-muenchen.de"
-SCANURL="${BASEURL}/fahrplanauskunft/fuer-entwickler/opendata/index.html"
-
-LOCATION=$(curl --connect-timeout 30 -s $SCANURL -o - | \
-           grep -E -i 'href="/fileadmin/mediapool/02-Fahrplanauskunft/03-Downloads/openData/.*?\.zip"'  | \
-           tail -1 | \
-           sed -e 's/^.*href="//i' \
-               -e 's/\.zip.*$/.zip/')
-
-if [ -n "$LOCATION" ]
-then
-    RELEASE_URL=$BASEURL$LOCATION
-fi
+RELEASE_URL="https://www.mvv-muenchen.de/fileadmin/mediapool/02-Fahrplanauskunft/03-Downloads/openData/gesamt_gtfs.zip"
 
 echo $RELEASE_URL
