8eedc07757f0606497bf90473ead61ac0e109813
Toni Erdmann <Toni.Erdmann@web.de>
2026-01-12 14:08:52 +0100

    improve error detection and reporting

--- FR/BRE/Star/get-release-date.sh
+++ FR/BRE/Star/get-release-date.sh
@@ -17,6 +17,26 @@ then
         then
             RELEASE_DATE=$result
         fi
+    else
+        curl --connect-timeout 30 -sI -v $RELEASE_URL 2>&1 | grep -E '(^HTTP/)|(SSL certificate problem)' > ./release_date_error.log
+
+        if [ -s ./release_date_error.log ]
+        then
+            if [ $(grep -c 'SSL certificate problem' ./release_date_error.log) -eq 1 ]
+            then
+                curl -k --connect-timeout 30 -sI $RELEASE_URL 2>&1 | grep -i 'Content-Type' > ./release_date_error.log
+
+                if [ $(grep -i -c 'Content-Type: text/html' ./release_date_error.log) -eq 1 ]
+                then
+                    curl -k --connect-timeout 30 -v $RELEASE_URL 2>&1 | \
+                    grep '<title>'                                    | \
+                    sed -e 's/^.*<title>//' -e 's/<\/title>.*$//'     > ./release_date_error.log
+                fi
+            else
+                curl -k -sI --connect-timeout 30 $RELEASE_URL 2>&1 | \
+                grep -E 'HTTP/[1-9] [0-9]{3}'                        > ./release_date_error.log
+            fi
+       fi
     fi
 fi
 
