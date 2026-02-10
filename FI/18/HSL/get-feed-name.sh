a84ef90ecd7df09de715c5bd0b7e41b0059b17f2
Toni Erdmann <Toni.Erdmann@web.de>
2026-02-10 16:19:52 +0100

    add GTFS feed FI-18-HSL

---
+++ FI/18/HSL/get-feed-name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#
+# print name of GTFS feed
+#
+
+echo "FI-18-HSL"
---
+++ FI/18/HSL/get-release-url.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+RELEASE_URL="https://infopalvelut.storage.hsldev.com/gtfs/hsl.zip"
+
+echo $RELEASE_URL
