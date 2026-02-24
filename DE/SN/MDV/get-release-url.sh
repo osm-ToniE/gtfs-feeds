d00a0b11bc9054b7a6717b1763bdf1b567a7bcc4
Toni Erdmann <Toni.Erdmann@web.de>
2026-02-24 12:56:38 +0100

    update release URL acc. to mail from MDV

--- DE/SN/MDV/get-release-url.sh
+++ DE/SN/MDV/get-release-url.sh
@@ -4,4 +4,6 @@
 # get URL to download latest GTFS feed
 #
 
-echo "https://www.mdv.de/media/file/87bab703"
+#echo "https://www.mdv.de/media/file/87bab703"
+
+echo "https://owncloud.mdv.de/s/JZkOJNwSb0RRyey/download"
