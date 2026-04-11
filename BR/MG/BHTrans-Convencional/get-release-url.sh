ebad5ecb60a55306af9ce66e487a19eff2d6650e
Toni Erdmann <Toni.Erdmann@web.de>
2026-04-11 17:04:47 +0200

    fix release URL by following location of 302

--- BR/MG/BHTrans-Convencional/get-release-url.sh
+++ BR/MG/BHTrans-Convencional/get-release-url.sh
@@ -4,4 +4,16 @@
 # get URL to download latest GTFS feed
 #
 
-echo "https://ckan.pbh.gov.br/dataset/77764a7e-63fc-4111-ace3-fb7d3037953a/resource/f0fa78dc-74c3-49fa-8971-c310a76a07fa/download/gtfsfiles.zip"
+PERMALINK="https://ckan.pbh.gov.br/dataset/77764a7e-63fc-4111-ace3-fb7d3037953a/resource/f0fa78dc-74c3-49fa-8971-c310a76a07fa/download/gtfsfiles.zip"
+
+LOCATION=$(curl --connect-timeout 30 -sI $PERMALINK | grep -F -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+if [ -n "$LOCATION" ]
+then
+    if [ "$(echo $LOCATION | grep -c -i 'GTFSBHTRANS')" == 1 ]
+    then
+        RELEASE_URL=$LOCATION
+    fi
+fi
+
+echo $RELEASE_URL
--- BR/MG/BHTrans-Suplementar/get-release-url.sh
+++ BR/MG/BHTrans-Suplementar/get-release-url.sh
@@ -4,4 +4,16 @@
 # get URL to download latest GTFS feed
 #
 
-echo "https://ckan.pbh.gov.br/dataset/af0c47bb-5a82-4ae1-874f-e45dea1397ff/resource/b2a9341e-4471-45cc-a8c0-11be805590bc/download/gtfsfiles.zip"
+PERMALINK="https://ckan.pbh.gov.br/dataset/af0c47bb-5a82-4ae1-874f-e45dea1397ff/resource/b2a9341e-4471-45cc-a8c0-11be805590bc/download/gtfsfiles.zip"
+
+LOCATION=$(curl --connect-timeout 30 -sI $PERMALINK | grep -F -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
+
+if [ -n "$LOCATION" ]
+then
+    if [ "$(echo $LOCATION | grep -c -i 'GTFSBHTRANS')" == 1 ]
+    then
+        RELEASE_URL=$LOCATION
+    fi
+fi
+
+echo $RELEASE_URL
