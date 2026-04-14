df3cf7f1c10b34b01ae16d0557d5cc8d70bbb360
Toni Erdmann <Toni.Erdmann@web.de>
2026-04-14 16:00:00 +0200

    improve release date evaluation
    - do not take Last-Modified from a non 200 response

--- US/NPS/BLT/get-release-date.sh
+++ US/NPS/BLT/get-release-date.sh
@@ -8,16 +8,28 @@ RELEASE_URL=$(./get-release-url.sh)
 
 if [ -n "$RELEASE_URL" ]
 then
-    LAST_MODIFIED=$(curl --connect-timeout 30 -sI $RELEASE_URL | grep -F -i 'last-modified:' | sed -e 's/^last-modified:\s*//i')
+    headerfile=$(mktemp --tmpdir=$PWD)
 
-    if [ -n "$LAST_MODIFIED" ]
+    curl --connect-timeout 30 -sI $RELEASE_URL | sed -e 's/\s*\r$//' > $headerfile
+
+    HTTPStatus=$(cat $headerfile | grep -E -i '^HTTP' | sed -e 's/[^0-9]*$//' -e 's|^HTTP/.*\s||')
+
+    if [ "$HTTPStatus" = "200" ]
     then
-        result=$(date -d "$LAST_MODIFIED" '+%Y-%m-%d')
-        if [ "$(echo $result | grep -c '^20[0-9][0-9]-[01][0-9]-[0123][0-9]$')" == 1 ]
+        LAST_MODIFIED=$(cat $headerfile | grep -E -i 'last-modified:' | sed -e 's/^last-modified:\s*//i')
+
+        if [ -n "$LAST_MODIFIED" ]
         then
-            RELEASE_DATE=$result
+            result=$(date -d "$LAST_MODIFIED" '+%Y-%m-%d')
+            if [ "$(echo $result | grep -c '^20[0-9][0-9]-[01][0-9]-[0123][0-9]$')" == 1 ]
+            then
+                RELEASE_DATE=$result
+            fi
         fi
+    else
+        HTTPStatus=$(cat $headerfile | grep -E -i '^HTTP' > ./release_date_error.log)
     fi
+    rm -f $headerfile
 fi
 
 echo $RELEASE_DATE
