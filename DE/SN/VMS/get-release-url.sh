adc3b7f3ae9685f8bfad0ae02476c985f351c5a1
Toni Erdmann <Toni.Erdmann@web.de>
2026-04-14 15:34:29 +0200

    fix release url from scan to permalink with redirect

--- DE/SN/VMS/get-release-url.sh
+++ DE/SN/VMS/get-release-url.sh
@@ -4,15 +4,13 @@
 # get URL to download latest GTFS feed
 #
 
-SCANURL="https://www.vms.de/vms/service/downloads/"
+PERMALINK="https://www.vms.de/GTFS_VMS.zip"
 
-# https://www.vms.de/wp-content/uploads/2025/04/GTFS_VMS.zip
-
-LOCATION=$(curl --connect-timeout 30 -s $SCANURL -o - | grep -E -i 'href="https://www.vms.de/wp-content/uploads/.*?/GTFS_VMS.*?\.zip"' | tail -1 | sed -e 's/^.*href="//i' -e 's/\.zip.*$/.zip/')
+LOCATION=$(curl --insecure --connect-timeout 30 -sI $PERMALINK | grep -F -i 'Location:' | sed -e 's/^Location:\s*//i' -e 's/\r$//')
 
 if [ -n "$LOCATION" ]
 then
-    RELEASE_URL="$LOCATION"
+    RELEASE_URL=$LOCATION
 fi
 
 echo $RELEASE_URL
