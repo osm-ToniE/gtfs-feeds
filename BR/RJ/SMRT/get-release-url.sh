ce764389d1bbf04d7c73529510de94fda6eed1c2
Toni Erdmann <Toni.Erdmann@web.de>
2026-05-01 14:29:00 +0200

    add network 'BR-RJ-SMRT'

---
+++ BR/RJ/SMRT/get-release-url.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+RELEASE_URL="https://www.arcgis.com/sharing/rest/content/items/8ffe62ad3b2f42e49814bf941654ea6c/data"
+
+echo $RELEASE_URL
