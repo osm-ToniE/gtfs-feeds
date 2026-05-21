143bdfd29b678ea7f5d17f37a463581b81b47d53
Toni Erdmann <Toni.Erdmann@web.de>
2026-05-21 21:31:03 +0200

    use 'last-modified' header

--- EE/GTFS/get-release-date.sh
+++ EE/GTFS/get-release-date.sh
@@ -8,25 +8,17 @@ RELEASE_URL=$(./get-release-url.sh)
 
 if [ -n "$RELEASE_URL" ]
 then
-    mkdir tempdir
+    LAST_MODIFIED=$(curl --connect-timeout 30 -sI $RELEASE_URL | grep -F -i 'last-modified:' | sed -e 's/^last-modified:\s*//i')
 
-    wget -q -O tempdir/gtfs.zip --user-agent="PTNA @ ptna.openstreetmap.de" $RELEASE_URL
-
-    if [ -f tempdir/gtfs.zip -a -s tempdir/gtfs.zip ]
+    if [ -n "$LAST_MODIFIED" ]
     then
-        result=$(unzip -l tempdir/gtfs.zip 2> ./release_date_error.log | awk '/20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]/ { print $2; }' | sort -u | tail -1)
+        result=$(date -d "$LAST_MODIFIED" '+%Y-%m-%d')
         if [ "$(echo $result | grep -c '^20[0-9][0-9]-[01][0-9]-[0123][0-9]$')" == 1 ]
         then
             RELEASE_DATE=$result
-
-            mv tempdir $RELEASE_DATE
-        else
-            if [ $(grep -c -i 'cannot find zipfile directory' ./release_date_error.log) -eq 1 ]
-            then
-                echo "Not a ZIP file" > ./release_date_error.log
-            fi
-            rm -rf tempdir
         fi
+    else
+        curl --connect-timeout 30 -sI $RELEASE_URL | grep -i '^HTTP/' > ./release_date_error.log
     fi
 fi
 
