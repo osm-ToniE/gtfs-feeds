c41c2a6ece803daba5a0c94dc6522717e63bb38e
Emmy D'Anello <ynerant@emy.lu>
2026-06-17 23:28:33 +0200

    New SNCF GTFS URL
    Signed-off-by: Emmy D'Anello <ynerant@emy.lu>

--- FR/SNCF/get-release-url.sh
+++ FR/SNCF/get-release-url.sh
@@ -4,4 +4,4 @@
 # get URL to download latest GTFS feed
 #
 
-echo "https://eu.ftp.opendatasoft.com/sncf/plandata/export-opendata-sncf-gtfs.zip"
+echo "https://eu.ftp.opendatasoft.com/sncf/plandata/Export_OpenData_SNCF_GTFS_NewTripId.zip"
