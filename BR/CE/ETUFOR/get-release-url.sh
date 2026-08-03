37cd07f1acb5b6c055ac15384e6a8ad99511e991
Toni Erdmann <osm-ToniE@web.de>
2026-08-03 16:16:05 +0200

    add GTFS from BR-CE-ETUFOR

---
+++ BR/CE/ETUFOR/get-release-url.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# get URL to download latest GTFS feed
+#
+
+RELEASE_URL="https://dados.fortaleza.ce.gov.br/dataset/d6f1e64c-aca3-4867-8f39-53b7c9c2d211/resource/7058bfbe-5ba2-45f4-9a91-af1508a7c05b/download/arquivo_google.zip"
+
+echo $RELEASE_URL
