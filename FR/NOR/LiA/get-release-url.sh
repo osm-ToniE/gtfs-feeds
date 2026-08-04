9a74ca4b100287898ab3910f6cd19055a91c2f92
Toni Erdmann <osm-ToniE@web.de>
2026-08-04 17:11:37 +0200

    do not grep for 'static'

--- FR/NOR/LiA/get-release-url.sh
+++ FR/NOR/LiA/get-release-url.sh
@@ -10,12 +10,11 @@ JSON_URL="https://transport.data.gouv.fr/api/datasets/$DATASET_ID"
 
 LOCATION=$(curl --connect-timeout 30 -s $JSON_URL -o -                                     | \
          jq -r '.resources[] | select(.format=="GTFS") | (.updated + "_" + .original_url)' | \
-         grep -F 'static'                                                                  | \
          sort                                                                              | \
          tail -1                                                                           | \
          sed -e 's/^.*Z_http/http/')
 
-if [ $(echo $LOCATION | grep -c '^http.*static') -eq 1 ]
+if [ -n "$LOCATION" ]
 then
     RELEASE_URL=$LOCATION
 fi
