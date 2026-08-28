3545c1bd490d9ede9f1fdc42ef1a2376e7bd5b2b
Toni Erdmann <osm-ToniE@web.de>
2026-08-28 11:14:37 +0200

    improve evaluation of RELEASE_DATE

--- FR/PAC/Mouvenbus/get-release-date.sh
+++ FR/PAC/Mouvenbus/get-release-date.sh
@@ -22,6 +22,30 @@ then
     else
         curl --connect-timeout 30 -sI $RELEASE_URL | grep -i '^HTTP/' > ./release_date_error.log
     fi
+
+    if [ -z "$RELEASE_DATE" ]
+    then
+        mkdir tempdir
+
+        wget -q -O tempdir/gtfs.zip $RELEASE_URL
+
+        if [ -f tempdir/gtfs.zip -a -s tempdir/gtfs.zip ]
+        then
+            if [ $(file tempdir/gtfs.zip | grep -c -F 'HTML') -gt 0 ]
+            then
+                grep -E '^<[hH][1-9]' tempdir/gtfs.zip | sed -e 's/^<h[1-9]>//i' -e 's|</h[1-9]>$||' -e 's/<p>.*$//' | grep -v -E '^$' > ./release_date_error.log
+            else
+                result=$(unzip -l tempdir/gtfs.zip | awk '/20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]/ { print $2; }' | sort -u | tail -1)
+                if [ "$(echo $result | grep -c '^20[0-9][0-9]-[01][0-9]-[0123][0-9]$')" == 1 ]
+                then
+                    RELEASE_DATE=$result
+
+                    mv tempdir $RELEASE_DATE
+                fi
+            fi
+        fi
+    fi
+
 fi
 
 echo $RELEASE_DATE
